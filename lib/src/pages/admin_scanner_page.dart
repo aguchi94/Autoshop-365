c3e8ae67d9f25a0cae091c37152755b96860b13b
EduardoSan365 <infocorreosc@gmail.com>
2022-08-31 15:05:37 -0300

    Actualizacion

--- lib/src/pages/admin_scanner_page.dart
+++ lib/src/pages/admin_scanner_page.dart
@@ -12,8 +12,7 @@ class AdminScannerPage extends StatefulWidget {
 class _AdminScannerPageState extends State<AdminScannerPage> {
   String _code = '';
   int counter = 0;
-  int quantitiProd = 0;
-
+  int counter2 = 0;
   void _cerrarSesion() => Navigator.of(context).pushReplacementNamed('/login');
 
   @override
@@ -28,7 +27,6 @@ class _AdminScannerPageState extends State<AdminScannerPage> {
                 if (counter == 0 && _code == "") {
                   setState(() {
                     _code = code;
-                    counter = 1;
                   });
                 }
               },
@@ -51,7 +49,7 @@ class _AdminScannerPageState extends State<AdminScannerPage> {
                               textAlign: TextAlign.center,
                               style: const TextStyle(
                                   fontSize: 30, fontWeight: FontWeight.bold)),
-                          subtitle: const Text("Precio",
+                          subtitle: const Text("Chocolinas",
                               textAlign: TextAlign.center,
                               style: TextStyle(
                                   color: Colors.green,
@@ -62,11 +60,12 @@ class _AdminScannerPageState extends State<AdminScannerPage> {
                     ),
                     const SizedBox(height: 25),
                     Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                      Text("   Control     "),
                       const SizedBox(width: 20),
                       FloatingActionButton(
                           child: Icon(Icons.remove),
                           onPressed: () {
-                            if (counter >= 2) {
+                            if (_code != "" && counter >= 1) {
                               setState(() => counter--);
                             } else {}
                           }),
@@ -78,8 +77,32 @@ class _AdminScannerPageState extends State<AdminScannerPage> {
                       FloatingActionButton(
                           child: Icon(Icons.add),
                           onPressed: () {
-                            if (counter >= 1 && counter < 8) {
+                            if (_code != "" && counter >= 0 && counter < 8) {
                               setState(() => counter++);
+                            } else {}
+                          }),
+                    ]),
+                    const SizedBox(height: 25),
+                    Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                      Text("Reposición"),
+                      const SizedBox(width: 20),
+                      FloatingActionButton(
+                          child: Icon(Icons.remove),
+                          onPressed: () {
+                            if (_code != "" && counter2 >= 1) {
+                              setState(() => counter2--);
+                            } else {}
+                          }),
+                      const SizedBox(width: 15),
+                      Text('$counter2',
+                          style: const TextStyle(
+                              fontSize: 25, fontWeight: FontWeight.bold)),
+                      const SizedBox(width: 15),
+                      FloatingActionButton(
+                          child: Icon(Icons.add),
+                          onPressed: () {
+                            if (_code != "" && counter2 >= 0 && counter2 < 8) {
+                              setState(() => counter2++);
                             } else {}
                           }),
                     ]),
@@ -108,18 +131,10 @@ class _AdminScannerPageState extends State<AdminScannerPage> {
                           style: ElevatedButton.styleFrom(
                               fixedSize: const Size(150, 45),
                               primary: Colors.green),
-                          child: const Text("Agregar",
+                          child: const Text("Enviar",
                               style: TextStyle(
                                   fontSize: 20, fontWeight: FontWeight.bold)),
-                          onPressed: () {
-                            if (counter >= 1) {
-                              setState(() {
-                                quantitiProd += counter;
-                                _code = "";
-                                counter = 0;
-                              });
-                            } else {}
-                          },
+                          onPressed: () {},
                         )
                       ],
                     ),
